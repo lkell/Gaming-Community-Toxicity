afcba655fdda483a44566e0d17965281b23445d1
Paul Wissler <u0791342@utah.edu>
2020-11-22 11:36:50 -0700

    updated data, added requirements for Python venv, updated summary-view

--- README.md
+++ README.md
@@ -1,7 +1,60 @@
 ﻿# Gaming-Community-Toxicity
  
- Find proposal here: https://docs.google.com/document/d/1CQiJoDG1dnF1_4Cy0uXYOsIm74Qw-bKKDAcXCxzYb0k/edit#
+README - The README file must give an overview of what you are handing in: which parts are your code, which parts are libraries, and so on. The README must contain URLs to your project websites and screencast videos. The README must also explain any non-obvious features of your interface.
+
+Find proposal here: https://docs.google.com/document/d/1CQiJoDG1dnF1_4Cy0uXYOsIm74Qw-bKKDAcXCxzYb0k/edit#
  
- Find process book here: https://docs.google.com/document/d/1c6NqrIbZIaTO6lEIoFvRx7uOhNRhHHb-7e5cdiad27g/edit#
+Find process book here: https://docs.google.com/document/d/1c6NqrIbZIaTO6lEIoFvRx7uOhNRhHHb-7e5cdiad27g/edit#
  
- For the process book, use template on first page for entries to maintain stylistic consistency.
+For the process book, use template on first page for entries to maintain stylistic consistency.
+
+## Website Description
+LINK TO GITHUB PAGE HERE
+
+LINK TO SCREENCAST YOUTUBE/VIMEO LINK HERE
+
+DESCRIBE WEBSITE, FOCUSING ON NON-OBVIOUS ASPECTS.
+
+## Python Libraries
+`pip install -r requirements.txt`
+
+## JavaScript Libraries
+All libraries are linked from outside sources. BREAK DOWN WHAT SOURCES WE ARE CURRENTLY USING.
+
+## Data Processing
+Used Python for data processing, can find files in ./data_processing/ directory. Use spec-file.txt to create a Python venv and run code with it. There are two data processing files, one to get our links data set (used primarily in graph view) and one to mine comment data through the Reddit API. 
+
+### Configuration Data
+
+#### local_user.yaml
+See template_user.yaml for required entries. Will require you to register for Reddit API credentials. Follow instructions on website (https://towardsdatascience.com/scraping-reddit-data-1c0af3040768), then copy template_user.yaml to create a local_user.yaml file, copying in necessary information from the Reddit API (necessary fields have same name as described in towardsdatascience tutorial website).
+
+#### properties_map.yaml
+Used to create hyperlinks data set, breaking out the properties column into 86 separate columns (specified here in the Data format section: https://snap.stanford.edu/data/soc-RedditHyperlinks.html).
+
+#### subreddits.yaml
+Specifies which subreddits we will be using on our website.
+
+### Link Data
+Run create_hyperlinks_dataset.py with venv. You will need to download data from https://snap.stanford.edu/data/soc-RedditHyperlinks.html, specifically the soc-redditHyperlinks-body.tsv and soc-redditHyperlinks-title.tsv files (see Files section on provided website). These files are far too large to post on GitHub, hence why we require you to download it yourselves. These files are currently configured to be ignored in our .gitignore file. The output of create_hyperlinks_dataset.py is ./data_processing/config/reddit-hyperlinks-body.json. 
+
+### Comment Data
+Run create_comment_dataset.py with venv. You will need to have registered for Reddit API credentials (see local_user.yaml section for instructions). This will output three data sets in the ./data_processing/config/ directory: THREE FILES HERE. This data gets the top 100 posts sorted by hot, top, and controversial, and their associated top-level comments. We then run a VADER analysis on this data using the nltk library (VADER description link: LINK HERE)as well as calculate an automated readability index (description: LINK HERE).
+
+## Visualization Code
+Runs from index.html, referencing our ./js/ directory for javascript static files. Stylings can be found in styles.css. Within the ./js/ directory are the node-view, ranked-view, and summary-view directories, as well as our node-plot.js and script.js files.
+
+### node-view
+Creates the node view. FILL OUT AND SPECIFY WHICH VISUALIZATIONS THIS DRIVES.
+
+### ranked-view
+Create the ranked view. FILL OUT AND SPECIFY WHICH VISUALIZATIONS THIS DRIVES.
+
+### summary-view
+Creates the summary view. FILL OUT AND SPECIFY WHICH VISUALIZATIONS THIS DRIVES.
+
+### node-plot.js
+EXPLAIN WHAT THIS DOES, AND WHY IT ISN'T IN node-view.
+
+### script.js
+OUR MAIN FILE, CONTAINING UTILS AND LOADING INFO. FILL OUT AND EXPLAIN.
--- data_processing/create_comment_dataset.py
+++ data_processing/create_comment_dataset.py
@@ -1,3 +1,5 @@
+# Used this as a reference: https://medium.com/@woneuy01_5877/reddit-comment-sentiment-analysis-a07cc852ac1c PJW
+
 import json
 import os
 import praw
@@ -12,103 +14,115 @@ from pprint import pprint
 
 
 user_path = os.path.join('.', 'config', 'local_user.yaml')
-target_file_path = os.path.join('.', 'config', 'reddit-sentiment-analysis.json')
+hot_comment_file_path = os.path.join('.', 'config', 'reddit-hot-comment-sentiment-analysis.json')
+controversial_comment_file_path = os.path.join('.', 'config', 'reddit-controversial-comment-sentiment-analysis.json')
+top_comment_file_path = os.path.join('.', 'config', 'reddit-top-comment-sentiment-analysis.json')
 subreddits_yaml_path = os.path.join('.', 'config', 'subreddits.yaml')
 
 with open(subreddits_yaml_path, 'r') as file:
-	subreddits = yaml.load(file, Loader=yaml.FullLoader)
-	# for i in range(len(source_subreddits)):
-	# 	source_subreddits[i] = source_subreddits[i].lower()
+    subreddits = yaml.load(file, Loader=yaml.FullLoader)
 
 with open(user_path, 'r') as file:
-	user_config = yaml.load(file, Loader=yaml.FullLoader)
+    user_config = yaml.load(file, Loader=yaml.FullLoader)
 
 reddit = praw.Reddit(
-	client_id=user_config['client_id'], 
-	client_secret=user_config['client_secret'],
-	user_agent = user_config['user_agent'],
+    client_id=user_config['client_id'], 
+    client_secret=user_config['client_secret'],
+    user_agent = user_config['user_agent'],
 )
-results = []
-for subreddit in subreddits:
-	print(subreddit)
-	hot_posts = reddit.subreddit(subreddit).hot(limit=100)
-	posts = []
-	for post in hot_posts:
-	    posts.append(
-	    	[
-		    	post.title, 
-		    	post.score, 
-		    	post.id, 
-		    	'https://www.reddit.com'+post.permalink,
-		    	post.subreddit, 
-		    	post.url, 
-		    	post.num_comments, 
-		    	post.selftext, 
-		    	dt.datetime.fromtimestamp(post.created)
-	    	])
 
-	posts = pd.DataFrame(posts,columns=[
-			'title', 
-			'score', 
-			'id', 
-			'permalink',
-			'subreddit', 
-			'url', 
-			'num_comments', 
-			'body', 
-			'created',
-		])
-	# posts.to_csv("reddit.csv")
-	ix = 0
-	for post_id in posts.id:
-		submission = reddit.submission(id=post_id)
-		timestamp = dt.datetime.fromtimestamp(int(submission.created_utc))
-		submission.comments.replace_more(limit=0)
-		# for top_level_comment in submission.comments:
-		# 	print(top_level_comment.body)
+def create_sentiment_yaml(target_file_path, reddit, subreddits, user_config, comment_type):
+    results = []
+    for subreddit in subreddits:
+        print(subreddit)
+        if comment_type == 'hot':
+            hot_posts = reddit.subreddit(subreddit).hot(limit=100)
+        elif comment_type == 'controversial':
+            hot_posts = reddit.subreddit(subreddit).controversial(limit=100)
+        else:
+            hot_posts = reddit.subreddit(subreddit).top(limit=100)
+        posts = []
+        for post in hot_posts:
+            posts.append(
+                [
+                    post.title, 
+                    post.score, 
+                    post.id, 
+                    'https://www.reddit.com'+post.permalink,
+                    post.subreddit, 
+                    post.url, 
+                    post.num_comments, 
+                    post.selftext, 
+                    dt.datetime.fromtimestamp(post.created)
+                ])
 
-		# submission.comments.replace_more(limit=0)
-		# for comment in submission.comments.list():
-		# 	print(comment.body)
+        posts = pd.DataFrame(posts,columns=[
+                'title', 
+                'score', 
+                'id', 
+                'permalink',
+                'subreddit', 
+                'url', 
+                'num_comments', 
+                'body', 
+                'created',
+            ])
 
-		comments = set()
-		# collect top level comment body
-		submission.comments.replace_more(limit=0)
-		for top_level_comment in submission.comments:
-		    comments.add(top_level_comment.body)
+        ix = 0
+        for post_id in posts.id:
+            submission = reddit.submission(id=post_id)
+            timestamp = dt.datetime.fromtimestamp(int(submission.created_utc))
+            submission.comments.replace_more(limit=0)
+            # for top_level_comment in submission.comments:
+            #   print(top_level_comment.body)
 
-		# nltk.download('vader_lexicon')
-		sia = SIA()
-		
-		for line in comments:
-			ix += 1
-			pol_score = sia.polarity_scores(line)
-			pol_score['AutoReadabilityIndex'] = textstat.automated_readability_index(line)
-			pol_score['subreddit'] = subreddit
-			pol_score['timestamp'] = timestamp
-			pol_score['ix'] = ix
-			results.append(pol_score)
+            # submission.comments.replace_more(limit=0)
+            # for comment in submission.comments.list():
+            #   print(comment.body)
+            comments = set()
+            # collect top level comment body
+            # submission.comments.replace_more(limit=0)
+            for top_level_comment in submission.comments:
+                if len(comments) <= 200:
+                    comments.add(top_level_comment.body)
+                else:
+                    break
 
-df = pd.DataFrame.from_records(results)
-df.timestamp = df.timestamp.astype(str)
-df.subreddit = df.subreddit.str.lower()
-df = df.rename(columns={
-	'pos': 'PositiveSentiment',
-	'neg': 'NegativeSentiment',
-	'compound': 'CompoundSentiment',
-})
-df = df.set_index(['subreddit', 'ix'])
+            # nltk.download('vader_lexicon')
+            sia = SIA()
+            
+            for line in comments:
+                ix += 1
+                pol_score = sia.polarity_scores(line)
+                pol_score['AutoReadabilityIndex'] = textstat.automated_readability_index(line)
+                pol_score['subreddit'] = subreddit
+                pol_score['timestamp'] = timestamp
+                pol_score['ix'] = ix
+                results.append(pol_score)
 
-def nest_df_into_json(df):
-	subreddits_ix = list(df.index.get_level_values(0).unique())
-	output_dict = dict()
-	for subreddit_ix in subreddits_ix:
-		interim_df = df.loc[subreddit_ix]
-		output_dict[subreddit_ix] = interim_df.to_dict(orient='index')
-	return output_dict
+    df = pd.DataFrame.from_records(results)
+    df.timestamp = df.timestamp.astype(str)
+    df.subreddit = df.subreddit.str.lower()
+    df = df.rename(columns={
+        'pos': 'PositiveSentiment',
+        'neg': 'NegativeSentiment',
+        'compound': 'CompoundSentiment',
+    })
+    df = df.set_index(['subreddit', 'ix'])
 
-nested_data = nest_df_into_json(df)
+    def nest_df_into_json(df):
+        subreddits_ix = list(df.index.get_level_values(0).unique())
+        output_dict = dict()
+        for subreddit_ix in subreddits_ix:
+            interim_df = df.loc[subreddit_ix]
+            output_dict[subreddit_ix] = interim_df.to_dict(orient='index')
+        return output_dict
 
-with open(target_file_path, 'w') as dump_data:
-    json.dump(nested_data, dump_data)
+    nested_data = nest_df_into_json(df)
 
+    with open(target_file_path, 'w') as dump_data:
+        json.dump(nested_data, dump_data)
+
+# create_sentiment_yaml(hot_comment_file_path, reddit, subreddits, user_config, 'hot')
+# create_sentiment_yaml(top_comment_file_path, reddit, subreddits, user_config, 'controversial')
+create_sentiment_yaml(controversial_comment_file_path, reddit, subreddits, user_config, 'top')
